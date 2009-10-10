3ebda44086318ce1e2f6ed05eb7313adb77daa7f
kenry52 <kenry52@05d1262b-b726-4fff-8b9f-3609a8a82e43>
2009-10-10 16:33:14 +0000

    forgot to add new java file...
    git-svn-id: svn+ssh://svn.code.sf.net/p/topiaryexplorer/code/trunk/main@142 05d1262b-b726-4fff-8b9f-3609a8a82e43

---
+++ src/topiarytool/VerticalTreeToolbar.java
@@ -0,0 +1,76 @@
+package topiarytool;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.event.*;
+
+
+public class VerticalTreeToolbar extends JToolBar {
+
+    JButton zoomOutButton  = new JButton("-");
+    JButton zoomInButton = new JButton("+");
+    JSlider zoomSlider = new JSlider(JSlider.VERTICAL, 0, 8, 0);
+    JPanel spacer1 = new JPanel();
+
+    MainFrame frame = null;
+
+    public VerticalTreeToolbar(MainFrame _frame) {
+
+        super(JToolBar.VERTICAL);
+        
+        frame = _frame;
+
+        zoomOutButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                zoomSlider.setValue(zoomSlider.getValue() - 1);
+                syncTreeWithZoomSlider();
+            }
+
+        });
+        zoomInButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                zoomSlider.setValue(zoomSlider.getValue() + 1);
+                syncTreeWithZoomSlider();
+            }
+
+        });
+        add(zoomInButton);
+        zoomSlider.setSnapToTicks(true);
+        zoomSlider.setPreferredSize(new Dimension(28,200));
+        zoomSlider.setMaximumSize(new Dimension(28,200));
+        zoomSlider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                if (zoomSlider.getValueIsAdjusting()){
+                    syncTreeWithZoomSlider();
+                }
+            }
+        });
+
+       
+
+
+        add(zoomSlider);
+        add(zoomOutButton);
+        setFloatable(false);
+    }
+
+    public void syncTreeWithZoomSlider() {
+        if (frame.tree.getTree() == null) return;
+        double minYScale = (frame.tree.getHeight() - 2*frame.tree.getMargin())/frame.tree.getTree().getNumberOfLeaves();
+        double newScale = minYScale * Math.pow(Math.pow(2.0, 0.5), zoomSlider.getValue());
+        frame.tree.setScaleFactor(frame.tree.getXScale(), newScale, frame.tree.getXStart(), frame.tree.height/2.0);
+    }
+
+    public void syncZoomSliderWithTree() {
+        if (frame.tree.getTree() == null) return;
+        double minYScale = (frame.tree.getHeight() - 2*frame.tree.getMargin())/frame.tree.getTree().getNumberOfLeaves();
+        int currStep = (int) Math.floor(Math.log(frame.tree.getYScale()/minYScale) / Math.log(Math.pow(2.0,0.5))+0.00001);
+        zoomSlider.setValue(currStep);
+    }
+
+}
