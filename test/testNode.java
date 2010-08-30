74d7d725b0086cd76a53787424e3bc61f94a794f
meganap <meganap@05d1262b-b726-4fff-8b9f-3609a8a82e43>
2010-08-30 17:22:07 +0000

    Adding tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/topiaryexplorer/code/trunk/main@268 05d1262b-b726-4fff-8b9f-3609a8a82e43

---
+++ test/testNode.java
@@ -0,0 +1,23 @@
+import junit.framework.TestCase;
+import topiaryexplorer.*;
+
+
+/**
+ * testNode
+ *
+ * @author megumi
+ * @since 08/04/10
+ */
+public class testNode extends TestCase {
+ 
+    private String treeString = "(a:0.10000000149011612,b:0.20000000298023224,(c:0.30000001192092896,d:0.4000000059604645,((e:0.10000000149011612,f:0.20000000298023224,(g:0.30000001192092896,h:0.4000000059604645,i:0.10000000149011612):0.5):0.0):0.8999999761581421):0.5):0.0";
+    private Node tree = TopiaryFunctions.createTreeFromNewickString(treeString);
+ 
+   public testNode(String name) {
+       super(name);
+   }
+ 
+   public void test() {
+      
+   }
+}
