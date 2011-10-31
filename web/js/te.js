6a14300016e6c443ec13281f74876b421b873c4f
meganap <meganap@05d1262b-b726-4fff-8b9f-3609a8a82e43>
2011-10-31 22:02:22 +0000

    Editing website
    git-svn-id: svn+ssh://svn.code.sf.net/p/topiaryexplorer/code/trunk/main@490 05d1262b-b726-4fff-8b9f-3609a8a82e43

--- web/js/te.js
+++ web/js/te.js
@@ -3,7 +3,16 @@ function showAbout()
     document.getElementById("about").className = "visible";
     document.getElementById("screenshots").className = "hidden";
     document.getElementById("tutorial").className = "hidden";
+    document.getElementById("paper").className = "hidden";
     
+}
+
+function showPaper()
+{
+    document.getElementById("paper").className = "visible";
+    document.getElementById("about").className = "hidden";
+    document.getElementById("screenshots").className = "hidden";
+    document.getElementById("tutorial").className = "hidden";
 }
 
 function showScreenshots()
@@ -11,6 +20,7 @@ function showScreenshots()
     document.getElementById("about").className = "hidden";
     document.getElementById("screenshots").className = "visible";
     document.getElementById("tutorial").className = "hidden";
+    document.getElementById("paper").className = "hidden";
     
 }
 
@@ -19,5 +29,6 @@ function showTutorial()
     document.getElementById("about").className = "hidden";
     document.getElementById("screenshots").className = "hidden";
     document.getElementById("tutorial").className = "visible";
+    document.getElementById("paper").className = "hidden";
     
 }
